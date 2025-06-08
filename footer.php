54c0873c86c8266e264298d629715ab0e1040bbf
Hazem Karam Sayed Ramadan <hazem.2123021@stemobour.moe.edu.eg>
2025-06-08 19:34:34 +0300

    Convert static site into basic WordPress theme

---
+++ footer.php
@@ -0,0 +1,31 @@
+      <div class="triangle-shape" class="down-sliding" id="footer-slide"></div>
+</section>
+<footer>
+  <div class="footer-container">
+    <a href="#"><h3 id="footer-logo"><span style="font-weight: bold">Noor</span>Class Academy</h3></a>
+    <div class="footer-column">
+      <h4 class="footer-column-title">Navigation</h4>
+      <ul class="footer-items">
+        <a href="#"><li class="footer-item">Home</li></a>
+        <a href="#"><li class="footer-item">About Us</li></a>
+        <a href="#"><li class="footer-item">Contact Us</li></a>
+        <a href="#"><li class="footer-item">Free Trial</li></a>
+      </ul>
+    </div>
+    <div class="footer-column">
+      <h4 class="footer-column-title">Contact Us</h4>
+      <ul class="footer-items">
+        <a href="mailto:noor.class.support@gmail.com"><li class="footer-item">noor.class.support@gmail.com</li></a>
+        <a href="tel:+201102401767"><li class="footer-item">+20 1102401767</li></a>
+      </ul>
+    </div>
+    <form action="#" class="newsletter-form">
+      <input type="email" class="text-field" placeholder="Email ..." />
+      <button class="sec-btn" type="submit">Subscribe</button>
+    </form>
+  </div>
+  <span id="love">Made with ❤ by <a href="#">Pyxelate</a></span>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
